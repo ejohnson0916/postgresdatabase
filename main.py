816c601d672b9efa62122335cbaba522b823e052
ejohnson0916 <ean.m.johnson1@gmail.com>
2021-02-15 16:06:22 -0500

    Building spotify dataframe, something wrong with conversion from list to dict with data/time

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 #Spotify username
 USER_NAME = "1269375672"
 # token from https://developer.spotify.com/console/get-recently-played/?limit=50&after=1484811043509&before=
-TOKEN = 'BQA7oKCjvN4210f7uxWVXeE9ZSj4if_F-GevFBdaPVtx_xX_Vquenx6PRovQasn_AfjWyayvsPh7tCO54PqYpfoJpgL_sZ7F_rcq9fwPkWPRC-4dSHLmdTHSqWJwUIV9zqorYlYOnBjp-CFVdUDv'
+TOKEN = 'BQDYx2Z5Zl-nq3_9f__BUE_hLmN0JP20AAJTkQY2jOkvEBickykggNiVJAjlQVObcDOI1YAtPS8OLyvSD7-TaP7ITMiXZSPIZuYcBBuJGbL5SEldj0xbsIhSGghXXT1lWwUzu7mKjHuYN9srKzxk'
 
 # Authorization is where the token goes, Not sure what Accept and Content Type args do
 header = {
@@ -29,14 +29,42 @@ today = datetime.datetime.now()
 # We want what I listened to yesterday so subtract the date/time right now by one day to get past 24hrs
 yesterday = today - datetime.timedelta(days=1)
 
-# Convert to unix format because that is what the Spotify API is in
+# Convert to unix format because that is what the Spotify API is in Unixtimne - number of seconds since epoch  ( 1
+# Jan 1970 ), Javascript "Data" object expects the number of milliseconds since the epoch, Hence multiply by 1000
+yesterday_unix_timestamp = int(yesterday.timestamp())
 
-print(yesterday)
+r = requests.get('https://api.spotify.com/v1/me/player/recently-played?after{time}'.format(time=yesterday_unix_timestamp), headers=header)
+data = r.json()
+
+# Lists that will be merged for a dataframe
+song_names = []
+artist_names = []
+played_at_list = []
+timestamps = []
+
+# I think this is basically looping through this object
+# Remember this syntax. I think that this is possible on all JSON objects.
+for song in data["items"]:
+    song_names.append(song['track']['name'])
+    artist_names.append(song['track']['album']['artists'][0]['name'])
+    played_at_list.append(song['played_at'])
+    timestamps.append(song['played_at'][0:10])
+
+print(played_at_list)
+# Turn the lists into a dictionary
+
+song_dict = {
+    'song_name': song_names,
+    'artist_name': artist_names,
+    'played_at': played_at_list,
+    'time_stamps': timestamps
+}
+
+print(song_dict['played_at'])
 
 
+# Put it all into a dataframe
+song_df = pd.DataFrame(data=song_dict, columns=['song_name', 'artist_name', 'exact_play_time', 'date_played'])
 
-
-#r = requests.get('https://api.spotify.com/v1/me/player/recently-played', headers=header)
-
-#print(r.json())
+#print(song_df.head())
 
