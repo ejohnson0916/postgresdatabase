ed1b1ccded88ba00a6fe173a812f53b28c9169a9
ejohnson0916 <ean.m.johnson1@gmail.com>
2021-02-18 09:10:57 -0500

    Fixed date time cols, data should be all clean

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 #Spotify username
 USER_NAME = "1269375672"
 # token from https://developer.spotify.com/console/get-recently-played/?limit=50&after=1484811043509&before=
-TOKEN = 'BQDYx2Z5Zl-nq3_9f__BUE_hLmN0JP20AAJTkQY2jOkvEBickykggNiVJAjlQVObcDOI1YAtPS8OLyvSD7-TaP7ITMiXZSPIZuYcBBuJGbL5SEldj0xbsIhSGghXXT1lWwUzu7mKjHuYN9srKzxk'
+TOKEN = 'BQAxkFEh_FTBpyhF1Lj1hQKtEScpW6JFdd0Ci09FOIw88cj-EJ0FpxDCTvwMD2WBYysOvWQQv_01kLTiULD4yiDBVNp_hbeX9GmpadrI0Odhf8JMRPMKumLiBFcEUa-BPfyg2CqZs0Zj2x6b1-NU'
 
 # Authorization is where the token goes, Not sure what Accept and Content Type args do
 header = {
@@ -31,7 +31,7 @@ yesterday = today - datetime.timedelta(days=1)
 
 # Convert to unix format because that is what the Spotify API is in Unixtimne - number of seconds since epoch  ( 1
 # Jan 1970 ), Javascript "Data" object expects the number of milliseconds since the epoch, Hence multiply by 1000
-yesterday_unix_timestamp = int(yesterday.timestamp())
+yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
 r = requests.get('https://api.spotify.com/v1/me/player/recently-played?after{time}'.format(time=yesterday_unix_timestamp), headers=header)
 data = r.json()
@@ -47,24 +47,28 @@ timestamps = []
 for song in data["items"]:
     song_names.append(song['track']['name'])
     artist_names.append(song['track']['album']['artists'][0]['name'])
+
+    # Code not right. Need to convert to datetime before we enter it into the dataframe
     played_at_list.append(song['played_at'])
     timestamps.append(song['played_at'][0:10])
 
-print(played_at_list)
-# Turn the lists into a dictionary
+# Converting the time lists into datetime format before entering into dictionary
+# timestamps = [time.replace('-', '') for time in timestamps] list comprehension, kind of like mutate in dplyr
+timestamps = pd.to_datetime(timestamps)
+played_at_list = pd.to_datetime(played_at_list)
 
+# Turn the lists into a dictionary
 song_dict = {
     'song_name': song_names,
     'artist_name': artist_names,
-    'played_at': played_at_list,
-    'time_stamps': timestamps
+    'timestamp': timestamps,
+    'played_at': played_at_list
 }
 
-print(song_dict['played_at'])
-
-
 # Put it all into a dataframe
-song_df = pd.DataFrame(data=song_dict, columns=['song_name', 'artist_name', 'exact_play_time', 'date_played'])
+song_df = pd.DataFrame(song_dict)
 
-#print(song_df.head())
+song_df.info()
+
+print(song_df.head(15))
 
